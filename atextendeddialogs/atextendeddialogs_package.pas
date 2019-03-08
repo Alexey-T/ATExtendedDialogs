7a657982c7ab63bdb1c9f5d770a31aab02dc7f98
Alexey-t <support@uvviewsoft.com>
2019-03-08 21:16:39 +0300

    add LPK file

---
+++ atextendeddialogs/atextendeddialogs_package.pas
@@ -0,0 +1,21 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit atextendeddialogs_package;
+
+{$warn 5023 off : no warning about unused units}
+interface
+
+uses
+  ATFontDialog, LazarusPackageIntf;
+
+implementation
+
+procedure Register;
+begin
+end;
+
+initialization
+  RegisterPackage('atextendeddialogs_package', @Register);
+end.
